1ed14c15821ace30e2ab20b2bcba7b787a662637
Maxime Lemesle <maxime.lemesle7@gmail.com>
2024-11-26 12:26:58 +0100

    Add /api at the base of the url

--- src/server.ts
+++ src/server.ts
@@ -11,14 +11,20 @@ const PORT = process.env.PORT || 8888;
 app.use(express.json());
 app.set('view engine', 'ejs');
 
+// Redirect to /api
+app.get('/', (req: Request, res: Response) => {
+  res.redirect('/api');
+});
+
+
 // Get home page
-app.get("/", (req: Request, res: Response): void => {
+app.get("/api", (req: Request, res: Response): void => {
   res.status(200).render('views');
 });
 
 // Routes user
 import user from './routes/user.routes.ts';
-app.use('/user', user);
+app.use('/api/user', user);
 
 
 // const album = {
@@ -28,21 +34,21 @@ app.use('/user', user);
 
 // Routes album
 import album from './routes/album.routes.ts';
-app.use('/album', album);
+app.use('/api/album', album);
 
 // const express = require('express')const app = express()const parkings = require('./parkings.json')// Middlewareapp.use(express.json())app.get('/parkings', (req,res) => {    res.status(200).json(parkings)})app.get('/parkings/:id', (req,res) => {    const id = parseInt(req.params.id)    const parking = parkings.find(parking => parking.id === id)    res.status(200).json(parking)})app.post('/parkings', (req,res) => {    parkings.push(req.body)    res.status(200).json(parkings)})app.listen(8080, () => {    console.log("Serveur à l'écoute")})
 
 // Routes page
 import page from './routes/page.routes.ts';
-app.use('/page', page);
+app.use('/api/page', page);
 
 // Routes comment
 import comment from './routes/comment.routes.ts';
-app.use('/comment', comment);
+app.use('/api/comment', comment);
 
 // Routes photo
 import photo from './routes/photo.routes.ts';
-app.use('/photo', photo);
+app.use('/api/photo', photo);
 
 // Start server
 app.listen(PORT, () => {
