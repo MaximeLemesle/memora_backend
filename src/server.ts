b6810b7e49e3cc48f499ba6f61116ee38e556bfd
Maxime Lemesle <maxime.lemesle7@gmail.com>
2024-12-08 01:34:41 +0100

    Update the server.ts and Fix error with dotenv import

--- src/server.ts
+++ src/server.ts
@@ -1,48 +1,48 @@
-import express, { Application, Request, Response } from 'express';
-import dotenv from 'dotenv';
-
-
-// Init server
-const app: Application = express();
+import express, { Application, Request, Response } from "express";
+import dotenv from "dotenv";
 dotenv.config();
-const PORT = process.env.PORT || 8888;
 
-// Middleware
-app.use(express.json());
-app.set('view engine', 'ejs');
+// Routes import
+import user from "./routes/user.routes.ts";
+import album from "./routes/album.routes.ts";
+import page from "./routes/page.routes.ts";
+import comment from "./routes/comment.routes.ts";
+import photo from "./routes/photo.routes.ts";
 
-// Redirect to /api
-app.get('/', (req: Request, res: Response) => {
-  res.redirect('/api');
-});
+function start() {
+  try {
+    // Init server
+    const app: Application = express();
+    const PORT = process.env.PORT || 8888;
 
+    // Middleware
+    app.use(express.json());
+    app.set("view engine", "ejs");
 
-// Get home page
-app.get("/api", (req: Request, res: Response): void => {
-  res.status(200).render('views');
-});
+    // Redirect to /api
+    app.get("/", (req: Request, res: Response) => {
+      res.redirect("/api");
+    });
 
-// Routes user
-import user from './routes/user.routes.ts';
-app.use('/api/user', user);
+    // Get home page
+    app.get("/api", (req: Request, res: Response): void => {
+      res.status(200).render("views");
+    });
 
-// Routes album
-import album from './routes/album.routes.ts';
-app.use('/api/album', album);
+    // End points
+    app.use("/api/user", user);
+    app.use("/api/album", album);
+    app.use("/api/page", page);
+    app.use("/api/comment", comment);
+    app.use("/api/photo", photo);
 
-// Routes page
-import page from './routes/page.routes.ts';
-app.use('/api/page', page);
+    // Start server
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error(`Error on the launching of the server : ${error}`);
+  }
+}
 
-// Routes comment
-import comment from './routes/comment.routes.ts';
-app.use('/api/comment', comment);
-
-// Routes photo
-import photo from './routes/photo.routes.ts';
-app.use('/api/photo', photo);
-
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+start();
